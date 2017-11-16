9e65182210c3bfce065332e6cad2eaa632171875
Rafael Torres <iterativo@users.noreply.github.com>
2017-11-16 09:03:09 -0600

    Add footer

--- src/footer.js
+++ src/footer.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
+// TODO Move to styles file
+const styles = {
+    footer: {
+        marginTop: '50px',
+    },
+    links: {
+        a: { color: '#27c59a' },
+        textTransform: 'uppercase',
+        display: 'flex',
+        justifyContent: 'space-between',
+        width: '45%',
+        margin: 'auto',
+        fontSize: '9px',
+    },
+};
+
 const Footer = () => (
-    <footer>
-        <div className="container">
-            TODO Add footer info
+    <footer style={styles.footer}>
+        <hr />
+        <div style={styles.links}>
+            <a href="#" style={styles.links.a}>about us</a>
+            <a href="#" style={styles.links.a}>support</a>
+            <a href="#" style={styles.links.a}>privacy</a>
+            <a href="#" style={styles.links.a}>terms</a>
+            <span>&#64; 2017 simplysocial</span>
         </div>
     </footer>
 );
