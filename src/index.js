dedfa613a5a02deeb7be207db6f98e789bfa67c9
Rafael Torres <iterativo@users.noreply.github.com>
2017-12-11 08:21:55 -0600

    Add redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import { render } from 'react-dom';
 
 import App from './app';
 
-render(<App />, document.getElementById('app'));
+/* eslint-disable no-underscore-dangle */
+const store = createStore(
+    (state=0, action) => { return state+1; },
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+/* eslint-enable */
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('app')
+);
